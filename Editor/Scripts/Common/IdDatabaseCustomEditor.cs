f466253c80947dc968403f0d1afbeac302ff5644
Qw1nt <pasharudnykh3@mail.ru>
2024-04-08 23:53:15 +0500

    [+] README.md

---
+++ Editor/Scripts/Common/IdDatabaseCustomEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Qw1nt.SelfIds.Editor.Scripts.Common
+{
+    [CustomEditor(typeof(IdsDatabase))]
+    public class IdDatabaseCustomEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}
